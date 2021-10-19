b098eadcd46eb5df037eb7b6ecf910da01108f64
Hildo paz <hildopaz1@gmail.com>
2021-10-19 14:25:20 -0300

    Testando

---
+++ HPBack/src/main/java/br/com/rd/HPBack/HpBackApplication.java
@@ -0,0 +1,13 @@
+package br.com.rd.HPBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HpBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HpBackApplication.class, args);
+	}
+
+}
---
+++ HPBack/src/test/java/br/com/rd/HPBack/HpBackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rd.HPBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HpBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
