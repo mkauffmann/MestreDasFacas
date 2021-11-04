543d0f8641186b5afff4ee3192cecbf168394dd4
JVCanada <jv.canada1@gmail.com>
2021-11-04 11:10:12 -0300

    email ocultando informações'

--- src/main/java/br/com/rd/MestreDasFacas/controller/EmailController.java
+++ src/main/java/br/com/rd/MestreDasFacas/controller/EmailController.java
@@ -3,6 +3,7 @@ package br.com.rd.MestreDasFacas.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,10 +14,11 @@ public class EmailController {
     @Autowired
     private JavaMailSender mailSender;
 
-    @RequestMapping(path = "/email-send", method = RequestMethod.GET)
+    @GetMapping("/email-send")
     public String sendMail() {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setText("Olá! Esse é um teste de Email");
+        message.setSubject("Confirmação de cadastro!");
         message.setTo("fazeroquen@gmail.com");
         message.setFrom("fazeroquen@gmail.com");
 
