9540db1a1d8fb018dbe9a6cdee4fef9aa1bc18f4
RafaelSilva <rafa.dev.silva@gmail.com>
2021-11-11 10:10:03 -0300

    ATUALIZANDO DEVELOP

--- src/main/java/br/com/rd/MestreDasFacas/repository/contract/EmailRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.rd.MestreDasFacas.repository.contract;
-
-import br.com.rd.MestreDasFacas.model.entity.EmailModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface EmailRepository extends JpaRepository<EmailModel, Long> {
-}
