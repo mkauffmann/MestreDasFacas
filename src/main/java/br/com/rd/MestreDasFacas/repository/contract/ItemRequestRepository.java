8edfe98429a003216a6930f4027a75658e064de8
RafaelSilva <rafa.dev.silva@gmail.com>
2021-11-12 16:13:36 -0300

    rafael

--- src/main/java/br/com/rd/MestreDasFacas/repository/contract/ItemRequestRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.rd.MestreDasFacas.repository.contract;
-
-import br.com.rd.MestreDasFacas.model.entity.ItemRequest;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemRequestRepository extends JpaRepository <ItemRequest, Long> {
-}
