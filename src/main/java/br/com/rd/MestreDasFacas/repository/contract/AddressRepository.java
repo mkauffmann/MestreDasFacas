eaee18b834b72dfec370f9bcab99287245412fa6
RafaelSilva <rafa.dev.silva@gmail.com>
2021-11-21 02:39:27 -0300

    limpar

--- src/main/java/br/com/rd/MestreDasFacas/repository/contract/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.rd.MestreDasFacas.repository.contract;
-
-import br.com.rd.MestreDasFacas.model.entity.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AddressRepository extends JpaRepository<Address, Long> {
-}
